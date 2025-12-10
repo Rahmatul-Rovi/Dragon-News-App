99dc2edfc017bef4a0063f725e8495b92bd85f0b
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-10 12:09:05 +0600

    categories section added

--- src/Components/Pages/CategoryNews.jsx
+++ src/Components/Pages/CategoryNews.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const {id} = useParams();
+    console.log(id);
     return (
         <div>
-            <h2>This is category news</h2>
+            <h2>This is category news {id}</h2>
         </div>
     );
 };

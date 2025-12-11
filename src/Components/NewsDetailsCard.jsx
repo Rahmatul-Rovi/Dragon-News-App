a43437e363842d6722904152a96ea3ec81f43742
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-12 00:09:28 +0600

    next page added

--- src/Components/NewsDetailsCard.jsx
+++ src/Components/NewsDetailsCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-const NewsDetailsCard = () => {
+const NewsDetailsCard = ({news}) => {
+    console.log(news);
     return (
-        <div>
-            News Details Card
+        <div className='space-y-5'>
+        <img className='w-full h-[400px] object-cover' src={news.image_url} alt="" />
+            <h2 className='text-2xl'>{news.title}</h2>
+            <p>{news.details}</p>
+            <Link className='btn btn-secondary' to={`/category/${news.category_id}`}>Back to Category</Link>
         </div>
     );
 };
